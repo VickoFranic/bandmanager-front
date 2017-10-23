203f314506b7c52fc46a15bb0a7dca37a69c1034
Vicko Franić <vicko@q-alliance.com>
2017-10-23 22:58:12 +0200

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private fb: FacebookService) {
+    let initParams: InitParams = {
+      appId: '1528884584098427',
+      xfbml: true,
+      version: 'v2.8'
+    };
+
+    fb.init(initParams);
+  }
+
+  loginWithFacebook(): void {
+    this.fb.login()
+      .then((response: LoginResponse) => console.log(response))
+      .catch((error: any) => console.log(error));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FacebookService } from 'ngx-facebook';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    FacebookService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
